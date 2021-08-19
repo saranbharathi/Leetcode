14629fe66862ebc64ce4cc8385c8000e1ef77aa4
saranbharathi <60891762+saranbharathi@users.noreply.github.com>
2021-08-19 14:54:33 +0530

    Update twosums.py

--- twosums.py
+++ twosums.py
@@ -10,21 +10,3 @@ nums=[5,3,1,5,2]
 target=7
 sum=Solution()
 sum.TwoSum(nums,target)
-
-"""
-#optimal solution(for the sum of values)
-class result:
-	def twosumhashing(self,nums,target):
-		sum=[]
-		hashtable={}
-		for i in range(len(nums)):
-			complement=target-nums[i]
-			if complement in hashtable:
-				print([nums[i],complement])
-			hashtable[nums[i]]=nums[i]
-
-nums=[5,3,1,5,2]
-target=7
-sol=result()
-sol.twosumhashing(nums,target)
-"""
